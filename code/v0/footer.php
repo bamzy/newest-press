d63a35c4fa1529a3e3e3fe1bcd935dbf867765e0
Wei Wei <wwei1@ualberta.ca>
2016-03-10 11:35:48 -0700

    Add header and footer to login page

---
+++ code/v0/footer.php
@@ -0,0 +1,31 @@
+<div class="footer">
+	<div class="footer-inner">
+		THANKS TO OUR SPONSORS
+		<span style="float: right;text-indent: -106px">CONTACT US</span>
+		<br>
+		
+		 <a class="cca" href="http://www.canadacouncil.ca/" ><img src="images/CCA-logo.png" title="Canadian Council for the Arts logo" /></a> 
+
+		 <a class="ch" href="http://www.pch.gc.ca/eng/1266037002102/1265993639778" ><img src="images/ch-logo.png" title="Canadian Heritage logo" /></a> <br>
+
+		 <a class="goa" href="http://www.alberta.ca/" ><img height="37px" src="images/AB-Gov Reverse.png" title="Government of Alberta logo" width="203px" /></a> 
+
+		 <a class="eac" href="http://www.edmontonarts.ca/"><img src="images/eac-logo.png" title="Edmonton Arts Council logo" /></a> <br>
+
+		 <a class="coe" href="http://www.edmonton.ca/" ><img src="images/coe-logo.png" title="City of Edmonton logo" /></a>
+
+		 <div class="footer-right" style="float: right;margin-top: -120px; font-style: italic;">
+		 	#201 8540 - 109 St.<br>
+			Edmonton, AB<br>
+			T6G 1E6<br>
+			<br>
+			(780) 432-9427 - phone<br>
+			(780) 433-3179 - fax
+		 </div>
+
+	</div>
+
+</div>
+
+</body>
+</html>
--- code/v0/login.php
+++ code/v0/login.php
@@ -31,14 +31,10 @@ return true;
 
 </script>
 
+<?php include'header.php' ?>
 
-</head>
-
-<body> 
-   <div id="wrap">
-		<div id="header"></div>
-		<div id="main">
-			
+		<div class="main" id="main">
+			<div id="sidebar"><?php include 'sidemenu.php' ?></div>
 			<p>Welcome to the NeWest Press Manuscript Tracking System. If you wish to submit a manuscript for consideration you must first <a href="./register.php">register</a> with the press. Once you have registered you can submit your manuscript information. You may also submit an electronic copy of your manuscript, although this is not required. You must mail us a printed copy of your manuscript, regardless of whether you submit an electronic manuscript. Hold on to your login, in the event that you wish to submit another manuscript in the future.</p>
 		
 			
@@ -50,12 +46,11 @@ return true;
 					<input type="submit" value="login">
 					<p> Or <a href="./register.php"> sign up here</a>. Accounts are free, and we never share your personal information with anyone, ever.</p>
 			</form></p> 
+			
 		</div> <!-- end main -->
-		<div id="sidebar"><?php include 'sidemenu.php' ?></div>
-		<div id="footer">footer stuff</div>
-    </div> <!-- end wrap -->
-</body>
-</html>
+		
+		<div class="clear"></div>
+<?php include'footer.php' ?>
 
 		
 			
